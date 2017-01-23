6a18b1e7f7f974afc7bd0687bd1da07fa702a09b
danieldunderfelt <dunderfeltdaniel@gmail.com>
2017-01-23 20:17:30 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component, PropTypes } from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import { observer } from 'mobx-react/native'
+import styled from 'styled-components/native'
+
+@observer
+class App extends Component {
+
+  render() {
+
+    return (
+      <View>
+
+      </View>
+    )
+  }
+}
+
+export default App
